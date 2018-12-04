f1ff8b19c40deae97cc0faecc2681b1d64e783aa
cyd <490181369@qq.com>
2018-12-04 16:55:32 +0800

    主程序

---
+++ flaskr.py
@@ -0,0 +1,148 @@
+#coding=utf-8
+# all the imports
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+import json
+import random
+
+from pyecharts import Scatter3D
+from pyecharts import Bar
+from pyecharts_javascripthon.api import TRANSLATOR
+app = Flask(__name__)
+
+
+REMOTE_HOST = "https://pyecharts.github.io/assets/js"
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+def scatter3d():
+    data = [generate_3d_random_point() for _ in range(80)]
+    range_color = [
+        "#313695",
+        "#4575b4",
+        "#74add1",
+        "#abd9e9",
+        "#e0f3f8",
+        "#fee090",
+        "#fdae61",
+        "#f46d43",
+        "#d73027",
+        "#a50026",
+    ]
+    scatter3D = Scatter3D("3D scattering plot demo", width=1200, height=600)
+    scatter3D.add("", data, is_visualmap=True, visual_range_color=range_color)
+    return scatter3D
+
+
+def generate_3d_random_point():
+    return [
+        random.randint(0, 100), random.randint(0, 100), random.randint(0, 100)
+    ]
+@app.route("/bar")
+def hellobar():
+    _bar = bar_chart()
+    javascript_snippet = TRANSLATOR.translate(_bar.options)
+    return render_template(
+        "pyecharts.1.html",
+        chart_id=_bar.chart_id,
+        host=REMOTE_HOST,
+        renderer=_bar.renderer,
+        my_width="100%",
+        my_height=600,
+        custom_function=javascript_snippet.function_snippet,
+        options=javascript_snippet.option_snippet,
+        script_list=_bar.get_js_dependencies(),
+    )
+
+
+def bar_chart():
+    bar = Bar("我的第一个图表", "这里是副标题")
+    bar.add(
+        "服装", ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"], [5, 20, 36, 10, 75, 90]
+    )
+    return bar
+
+@app.route("/pyecharts")
+def hello():
+    s3d = scatter3d()
+    return render_template(
+        "pyecharts.html",
+        myechart=s3d.render_embed(),
+        host=REMOTE_HOST,
+        script_list=s3d.get_js_dependencies(),
+    )
+
+@app.route('/')
+def show_entries():
+
+    g.sqlite_db = get_db()
+    cur = g.sqlite_db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    g.db = get_db()
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)   
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+if __name__ == '__main__':
+    app.debug = True
+    app.config['DATABASE'] = './sqlite_db'
+    app.config['USERNAME'] = 'a'
+    app.config['PASSWORD'] = 'a'
+    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+    app.run()
